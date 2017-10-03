cfe6269103cd5b98fac8883b49f9f842d77528c8
Adam Kern <kernadam2@gmail.com>
2017-10-03 23:38:28 +0200

    shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FreelancerGridComponent } from './components/freelancer-grid/freelancer-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FreelancerGridComponent
   ],
   imports: [
     BrowserModule,
