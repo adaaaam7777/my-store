4d2382e30c3d687fe499b8d38674cb352b93db62
Adam Kern <kernadam2@gmail.com>
2017-10-24 02:04:45 +0200

    mapped

--- src/app/components/services/exchange-api.service.ts
+++ src/app/components/services/exchange-api.service.ts
@@ -4,6 +4,7 @@ import { Http } from '@angular/http';
 import { COIN_ACTIONS } from '../../reducers/coins.reducer';
 import { AppState } from '../../reducers/index';
 import { EXCHANGES_ACTIONS } from '../../reducers/exchanges.reducer';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ExchangeApiService {
@@ -12,7 +13,20 @@ export class ExchangeApiService {
 	private BITTREX_URL = 'https://bittrex.com/api/v1.1/public/getmarketsummaries';
 	private KRAKEN_URL = 'https://api.kraken.com/0/public/Ticker';
 
-	constructor( private store: Store<AppState>, private http: Http ) { }
+	coinData: Observable<any>;
+
+	constructor( private store: Store<AppState>, private http: Http ) {
+		this.coinData =  this.http.request( 'src/app/data/currencies/currencies.json' )
+			.map( res => res.json() )
+			.map( ( response ) => {
+				return response.map( ( coinData ) => {
+					return { [ coinData.value ]: coinData.name };
+				} );
+			} )
+			.map ( ( response ) => {
+				return Object.assign( [], ...response );
+			} );
+	}
 
 	private toCoin( value: any ) {
 		return {
@@ -41,8 +55,10 @@ export class ExchangeApiService {
 
 	getBittrexCoins() {
 		return this.http.get( `${this.BITTREX_URL}` )
-			.map( ( response ) => {
-				// return this.mapBittrex( response );
+			.withLatestFrom( this.coinData )
+			.map( ( [ response, coins ] ) => {
+				console.log( 'mapped: ', this.mapBittrex( response, coins ) );
+				return this.mapBittrex( response, coins );
 			} );
 			/*this.store.dispatch( {
 				type: EXCHANGES_ACTIONS.EXCHANGES_COINS_LOADED,
@@ -52,29 +68,35 @@ export class ExchangeApiService {
 			} );*/
 	};
 
-/*	mapBittrex( coinData ) {
+	mapBittrex( coinData, coins ) {
+		console.log( 'coins: ', coins );
 		console.log( 'BITTREX:  ', coinData.json().result );
-		return coinData.json().result.map( ( bittrexCoin ) => {
-			return {
-				name: bittrexCoin.name,
-				rank: value.rank,
-				thumbnail: value.url,
-				symbol: value.symbol,
-				price_usd: value.price_usd,
-				price_btc: value.price_btc,
-				_24_h_volume_usd: value['24h_volume_usd'],
-				market_cap_usd: value.market_cap_usd,
-				available_supply: value.available_supply,
-				total_supply: value.total_supply,
-				percent_change_1h: value.percent_change_1h,
-				percent_change_24h: value.percent_change_24h,
-				percent_change_7d: value.percent_change_7d,
-				last_updated: value.last_updated
-			};
+		return coinData.json().result
+			.filter( ( bittrexCoin ) => { // ony BTC market( so wont have dupes...)
+				return bittrexCoin.MarketName.split( '-' )[ 0 ] === 'BTC';
+			} )
+			.map( ( bittrexCoin ) => {
+				let coinSymbol = bittrexCoin.MarketName.split( '-' )[ 1 ]
+				return {
+					name: coins[ coinSymbol ],
+					rank: null,
+					thumbnail: null,
+					symbol: coinSymbol/*,
+					price_usd: value.price_usd,
+					price_btc: value.price_btc,
+					_24_h_volume_usd: value['24h_volume_usd'],
+					market_cap_usd: value.market_cap_usd,
+					available_supply: value.available_supply,
+					total_supply: value.total_supply,
+					percent_change_1h: value.percent_change_1h,
+					percent_change_24h: value.percent_change_24h,
+					percent_change_7d: value.percent_change_7d,
+					last_updated: value.last_updated*/
+				};
 		} );
-	}*/
+	}
 
-	getCoinValues() {
+/*	getCoinValues() {
 		return this.http.get( 'https://api.coinmarketcap.com/v1/ticker/' )
 			.map( ( data )  =>  {
 				return data.json();
@@ -86,7 +108,6 @@ export class ExchangeApiService {
 						symbol: coin.symbol
 					};
 				} );
-			} )
-			.do( ( data ) => console.log( data ) );
-	}
+			} );
+	}*/
 }
